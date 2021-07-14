48e7ad220999f4ef8c5ca5c6ec32db0b1762ad07
Ryan Noronha <ryannor56a@icloud.com>
2021-07-14 17:29:22 +0530

    Add a new express server

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+
+const server = app.listen(5000, () => {
+  console.log('Express server listening on port 5000');
+});
+
+/**
+ * Gracefully shuts down the ExpressJS server.
+ */
+function gracefulShutdown() {
+  console.log('Shutting down server');
+
+  server.close((error) => {
+    if (error) {
+      console.error(`Error while shutting down server: ${error}`);
+    } else {
+      console.log('Server shutdown successful');
+    }
+  });
+}
+
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGHUP', gracefulShutdown);
